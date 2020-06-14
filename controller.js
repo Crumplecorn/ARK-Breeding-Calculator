474f3612374a1b269dcfcd5fdaaf1b2285d5d4cd
Tom Hoogstra <tomhoogstra@live.com.au>
2020-06-15 01:27:07 +1000

    Added deinonychus

--- controller.js
+++ controller.js
@@ -444,6 +444,19 @@ var breedingController=angular.module('breedingControllers', []).controller('bre
 				"Cooked Meat": 0
 			}
 		},
+		
+		Deinonychus: {
+			birthtype: "Incubation",
+			type: "Carnivore",
+			basefoodrate: 0.001543,
+			babyfoodrate: 25.5,
+			extrababyfoodrate: 20,
+			agespeed: 0.000003,
+			agespeedmult: 2.5,
+			eggspeed: 0.005556,
+			eggspeedmult: 3.0,
+			weight: 140.0
+		},
 
 		Dilophosaurus: {
 			birthtype: "Incubation",
